98279b94e5b36c530bd7d00ff66ab2109c4f1edf
Hiago Popolin <hiago.popolin@outlook.com>
2022-11-04 22:27:52 -0300

    teste

--- main.js
+++ main.js
@@ -19,24 +19,20 @@ function createCard(date, day, games) {
           </div>
   `
 }
-document.querySelector("#cards").innerHTML = 
-createCard(
-  "20/11",
-  "domingo",
-  createGame("qatar", "13:00", "ecuador")
-)+
-createCard(
-  "21/11",
-  "segunda",
-  createGame("england", "10:00", "iran") +
-    createGame("senegal", "13:00", "netherlands") +
-    createGame("unitedstates", "16:00", "netherlands")
-)+
-createCard(
-  "24/11",
-  "quinta",
-  createGame("brazil", "16:00", "serbia") +
-    createGame("portugal", "16:00", "uruguay")
-) +
+document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("england", "10:00", "iran") +
+      createGame("senegal", "13:00", "netherlands") +
+      createGame("unitedstates", "16:00", "wales")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("brazil", "16:00", "serbia") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
   createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland")) +
   createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
